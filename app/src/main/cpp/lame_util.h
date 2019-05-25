076a7589d0080cc55ab483e42487f4bce769a039
Kosmos <KosmoSakura@gmail.com>
2019-05-25 15:11:24 +0800

    cmake，头文件 构建

---
+++ app/src/main/cpp/lame_util.h
@@ -0,0 +1,45 @@
+/*
+#include <jni.h>
+
+extern "C" {
+#endif
+*/
+/*
+ * Class:     Java_cos_mos_recorder_decode_ULame
+ * Method:    init
+ * Signature: (IIIII)V
+ *//*
+
+JNIEXPORT void JNICALL
+Java_cos_mos_recorder_decode_ULame_init(JNIEnv *, jclass, jint, jint, jint, jint, jint);
+
+*/
+/*
+ * Class:     Java_cos_mos_recorder_decode_ULame
+ * Method:    encode
+ * Signature: ([S[SI[B)I
+ *//*
+
+JNIEXPORT jint JNICALL
+Java_cos_mos_recorder_decode_ULame_encode(JNIEnv *, jclass, jshortArray, jshortArray, jint,
+                                          jbyteArray);
+
+*/
+/*
+ * Class:     Java_cos_mos_recorder_decode_ULame
+ * Method:    flush
+ * Signature: ([B)I
+ *//*
+
+JNIEXPORT jint JNICALL Java_cos_mos_recorder_decode_ULame_flush(JNIEnv *, jclass, jbyteArray);
+
+*/
+/*
+ * Class:     Java_cos_mos_recorder_decode_ULame
+ * Method:    close
+ * Signature: ()V
+ *//*
+
+JNIEXPORT void JNICALL Java_cos_mos_recorder_decode_ULame_close(JNIEnv *, jclass);
+}
+*/
